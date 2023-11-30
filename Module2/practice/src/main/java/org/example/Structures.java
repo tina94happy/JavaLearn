6cc29ef92caad2288a4c1768a6a939e65db20ab6
tina94happy <83741238+tina94happy@users.noreply.github.com>
2023-11-30 15:29:12 +0800

    Note - println will print value in newline

--- Module2/practice/src/main/java/org/example/Structures.java
+++ Module2/practice/src/main/java/org/example/Structures.java
@@ -8,6 +8,7 @@ public class Structures {
         int i, j;
         for (i=1;i<10;i++){
             for (j=1;j<10;j++){
+                // don't use println that's different from print
                 System.out.print(i + "x" + j + "=" + i * j + "\t");
             }
             System.out.println();
